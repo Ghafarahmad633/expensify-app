d9029ac27b77a5d172f6736779c1f5c6741c96c7
Ghafarahmad633 <ghafarahmad633@gmail.com>
2019-05-23 10:11:21 +0500

    fixed single exnesify date format

--- src/component/ExpenseListItem.js
+++ src/component/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {removeExpense} from '../actions/expenses'
 import {Link} from "react-router-dom";
+import moment from 'moment'
  export const ExpenseListItem=({dispatch,desc,id,note,createdAt,amount,onSelectChange,selectAll})=>{
     console.log("This is from "+selectAll)
     return(
@@ -9,7 +10,12 @@ import {Link} from "react-router-dom";
             <Link  to={`/edit/${id}`}>
                 <h3>{desc}</h3>
             </Link>
-            <p>Created At {createdAt} - ${amount}</p>
+            <p>
+                Created At {createdAt}
+                </p>
+            <p>
+                {moment(createdAt).format("MMMMM","Do","YYYY")}
+            </p>
             <strong>{note}</strong>
             <input
                 type='checkbox'
